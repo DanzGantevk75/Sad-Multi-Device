07edde0ebd9274e762a96126e438cb0d7a2fdaca
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-07-13 18:51:01 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg*\nSubscribe Untuk Mengetahui Update Selanjutnya:https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg\nSilahkan Subscribe Channel saya dibawah ,Terimakasih`
-conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
+let str = `*Script Ini Di Private,Jika Ingin Tau Lebih Lanjut Chat Owner* wa.me/6288270863279`
+conn.sendButtonDoc(m.chat, str, wm, 'Yahahah','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
 handler.tags = ['info']
